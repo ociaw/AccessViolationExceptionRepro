f15cfc422cf5323d06d5b02c81f4af82895061ec
Michael Johnson <michael@ociaw.com>
2020-07-08 14:28:19 -0500

    Simplify by using a generic method instead of a constructor

--- Program.cs
+++ Program.cs
@@ -1,24 +1,15 @@
-namespace Repro
+class Program
 {
-    class Program
+    static void Main()
     {
-        static void Main()
-        {
-            new Generic<Implementation>(new Implementation());
-        }
-    }
-
-    public interface IInterface { }
-
-    struct Implementation : IInterface { }
-
-    public sealed class Generic<T> where T : IInterface
-    {
-        private readonly T _implementation;
-
-        public Generic(T implementation)
-        {
-            _implementation = implementation;
-        }
+        Class.Generic(new Implementation());
     }
 }
+public static class Class
+{
+    public static void Generic<T>(T implementation) where T : IInterface { }
+}
+
+public interface IInterface { }
+
+struct Implementation : IInterface { }
